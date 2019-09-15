b4692d904b0432ef47c8ed5d1264f2487e842f7f
Andre Bargas <ahbargas@latam.stefanini.com>
2019-09-15 09:49:15 -0300

    AGORA VAI

--- activities/service/api/bo/preprocess.py
+++ activities/service/api/bo/preprocess.py
@@ -4,6 +4,6 @@ import base64
 def preprocessmessage(message):
     print(message)
     n_message = base64.b64decode(message).decode("utf-8")
-    n_message = n_message.encode().hex()
+    n_message = n_message.encode().hex().split('0')
     print(n_message)
     return n_message[1:]
