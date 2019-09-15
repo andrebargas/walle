db5a819674bdd9b3e398a0b81d273ebdb03eeb3a
Andre Bargas <ahbargas@latam.stefanini.com>
2019-09-15 09:44:27 -0300

    hora e a agora

--- activities/service/api/bo/preprocess.py
+++ activities/service/api/bo/preprocess.py
@@ -1,6 +1,9 @@
+import base64
+
 
 def preprocessmessage(message):
     print(message)
-    n_message = message.encode().hex()
+    n_message = base64.b64decode(message).decode("utf-8")
+    n_message = n_message.encode().hex()
     print(n_message)
     return n_message[1:]
