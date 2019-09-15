8d7108db45dedb67d3eedc3f7f551033a69434ed
Andre Bargas <ahbargas@latam.stefanini.com>
2019-09-15 09:40:17 -0300

    agora e a hora

--- activities/service/api/bo/preprocess.py
+++ activities/service/api/bo/preprocess.py
@@ -1,4 +1,6 @@
 
 def preprocessmessage(message):
-    n_message = message.encode().hex().split("0")
+    print(message)
+    n_message = message.encode().hex()
+    print(n_message)
     return n_message[1:]
